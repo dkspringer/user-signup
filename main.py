d98770e2dfac5eb2bc636251a32e234aa78f1dfe
Dave Springer <david.k.springer@colorado.edu>
2018-12-05 19:14:55 -0500

    Added password verification check

--- main.py
+++ main.py
@@ -2,10 +2,26 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+def check_password_match(password, verify):
+	return password == verify
+
+def check_valid_username(username):
+	if len(username) < 3:
+		return 'Username may not be shorter 3 characters'
+	if len(username) > 20:
+		return 'Username may not be longer than 20 characters'
+	if ' ' in username:
+		return 'Username may not contain spaces'
+	return ''
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
 	if request.method == 'POST':
 		username = request.form.get('username')
+		password = request.form.get('password')
+		verify_password = request.form.get('verify')
+		email = request.form.get('email')
+
 		return render_template('welcome.html', name=username)
 	else:
 		return render_template('home.html')
